c32b92ac4804b44ffa8a8e15707f323bbef2fbf1
camilovivas <47890754+camilovivas@users.noreply.github.com>
2020-02-11 22:57:26 -0500

    start interface and change somethings in the class Matrix

--- src/model/Matrix.java
+++ src/model/Matrix.java
@@ -2,28 +2,34 @@ package model;
 
 public class Matrix {
 
-	private int n;
-	private Number[][] m;
+	private int number;
+	private int option;
+	private Number[][] numbers;
 
-	public Matrix(int n) {
-		this.n = n;
-		m = new Number[row()][column()];
+	public Matrix(int n, int o) {
+		this.number = n;
+		this.option = o;
+		numbers = new Number[row()][column()];
+		fillMatrix();
 	}
-
+	
+	//generate row matrix
 	public int row() {
-		return (int) Math.sqrt(n);
+		return (int) Math.sqrt(number);
 	}
-
+	
+	//generate column matrix
 	public int column() {
-		int reference = (int) Math.sqrt(n);
-		if (reference * row() == n) {
-			return (int) Math.sqrt(n);
+		int reference = (int) Math.sqrt(number);
+		if (reference * row() == number) {
+			return (int) Math.sqrt(number);
 		} else {
-			return (int) Math.sqrt(n) + 1;
+			return (int) Math.sqrt(number) + 1;
 		}
 	}
 	
-	public boolean primeNumber(int n) {
+	//method 1
+	public boolean primeNumber1(int n) {
 		if (n <= 1) {
 			return false;
 		}
@@ -34,23 +40,49 @@ public class Matrix {
 		}
 		return true;
 	}
+	
+	//method 2
+	
+	//TODO
+	
+	//method 3
+	
+	//TODO
 
+//	generate values matrix
 	public void fillMatrix() {
-		int number = 1;
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
-				if (number <= n) {
-					m[i][j] = new Number();
-					m[i][j].setValue(number);
-					if (primeNumber(number) == true) {
-						m[i][j].setPrime(true);
-					}
+		int number2 = 1;
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers[i].length; j++) {
+				if (number2 <= number) {
+					numbers[i][j] = new Number();
+					numbers[i][j].setValue(number2);
+					number++;
 				}
-				number++;
 			}
 		}
 	}
+	
+	public void option(int option) {
+		switch(option) {
+		case 1:
+			method1();
+		case 2:
+			
+		case 3:
+			
+		}
+	}
 
-
-
+	public void method1() {
+		for (int i = 0; i < numbers.length; i++) {
+			for (int k = 0; k < numbers[0].length; k++) {
+				int val = numbers[i][k].getValue();
+				if (primeNumber1(val) == true) {
+					numbers[i][k].setPrime(true);
+				}
+			}
+		}
+	}
+			
 }
